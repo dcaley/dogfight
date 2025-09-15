6de5f681cd1dab89075b2e30f63885306b333020
dcaley <davidcaley@gmail.com>
2025-09-14 22:07:04 -0700

    Upgrading dependencies

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "42964a8600b5c912d37b1da96ac4394b",
+"flutter_bootstrap.js": "97e277dd73f9067d9552308caba232c2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
